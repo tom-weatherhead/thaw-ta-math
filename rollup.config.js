e826b3f49f858b3a1ca0e59d13ce3deb7d5652bb
Tom Weatherhead <tomw@2hrd4u.org>
2021-12-02 15:30:34 -0700

    Dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,41 @@
 // rollup.config.js
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { terser } = require('rollup-plugin-terser');
+/**
+ * Copyright (c) Tom Weatherhead. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
 
-export default [
-	{
-		input: './dist/types/main.js',
-		output: [
-			{
-				file: 'dist/thaw-ta-math.cjs.js',
-				format: 'cjs',
-				exports: 'named'
-			},
-			{
-				file: 'dist/thaw-ta-math.esm.js',
-				format: 'es',
-				compact: true,
-				plugins: [terser()]
-			},
-			{
-				file: 'dist/thaw-ta-math.js',
-				name: 'thaw-ta-math',
-				format: 'umd',
-				compact: true,
-				plugins: [terser()]
-			}
-		]
-	}
-];
+'use strict';
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+	input: './dist/types/main.js',
+	output: [
+		{
+			file: 'dist/thaw-ta-math.cjs.js',
+			format: 'cjs',
+			exports: 'named'
+		},
+		{
+			file: 'dist/thaw-ta-math.esm.js',
+			format: 'es',
+			esModule: true,
+			compact: true,
+			plugins: [terser()]
+		},
+		{
+			file: 'dist/thaw-ta-math.js',
+			name: 'thaw-ta-math',
+			format: 'umd',
+			compact: true,
+			plugins: [terser()]
+		}
+	],
+	context: 'this',
+	// external: ['uuid'],
+	plugins: [nodeResolve()]
+};
